53e2a090f305c574d388d799409023de87958e55
Pulkit Mahour <90045706+pulkitMahour@users.noreply.github.com>
2024-05-16 18:41:11 +0530

    Update settings.py change the database to sqlite from postgress because of the deployment

--- drf_project/settings.py
+++ drf_project/settings.py
@@ -78,24 +78,24 @@ WSGI_APPLICATION = 'drf_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'BA*1*3A22GcF3BDBBEdaG4c3E22FBGBB',
-        'HOST': 'roundhouse.proxy.rlwy.net',
-        'PORT': '42353',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'railway',
+#         'USER': 'postgres',
+#         'PASSWORD': 'BA*1*3A22GcF3BDBBEdaG4c3E22FBGBB',
+#         'HOST': 'roundhouse.proxy.rlwy.net',
+#         'PORT': '42353',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
